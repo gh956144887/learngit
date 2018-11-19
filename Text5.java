cf926c1a4926a2176e5c9d6cefe62faeda55f7da
gh1 <956144887@qq.com>
2018-11-19 18:05:28 +0800

    初始

---
+++ Text5.java
@@ -0,0 +1,19 @@
+package √Ê ‘;
+
+
+public class Text5 {
+	public static void main(String[] args) {
+		Text5 text5 = new Text5();
+		text5.ch(null);
+	}
+	
+	
+	public void ch(String st){
+		System.out.println("123");
+	}
+	public void ch(Object ob){
+		System.out.println("456");
+	}
+	
+	
+}
